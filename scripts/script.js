404fa2470157e8acb7461d49d047c3bd8624ff3d
LuhxEn <axngrsantos@gmail.com>
2022-10-15 10:16:19 +0800

    Add sign-up page layout with simple password matching validation

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+let errorPrompt = document.querySelector('#error-prompt');
+let password = document.querySelector('#password');
+let passwordConfirm = document.querySelector('#password-confirm');
+
+function validatePassword(){
+  console.log('text');
+  if(password.value !== passwordConfirm.value){
+    errorPrompt.textContent = 'Passwords do not match';
+    return false
+  }else{
+    return true;
+  }
+}
