6269d4c79f05a2e6492a903dd8b71f0ed502036f
LuhxEn <axngrsantos@gmail.com>
2022-10-17 10:39:36 +0800

    Revise form labels for screen reader accessibility

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,8 @@ let passwordConfirm = document.querySelector('#password-confirm');
 function validatePassword(){
   console.log('text');
   if(password.value !== passwordConfirm.value){
-    errorPrompt.textContent = 'Passwords do not match';
+    errorPrompt.textContent = '*Passwords do not match';
+    errorPrompt.setAttribute('style', 'color:red;');
     return false
   }else{
     return true;
